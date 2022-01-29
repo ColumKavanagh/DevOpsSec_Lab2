c3ae32597a0d4c5fa4c2d9b65aa5970a89f995ea
Colum Kavanagh <colum.j.kavanagh@gmail.com>
2022-01-29 16:04:18 +0000

    updated script.sh with completion message

--- script.sh
+++ script.sh
@@ -5,3 +5,4 @@ mkdir fol_1 fol_2
 touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
 chmod 600 fol_*/*_1.txt fol_*/*_3.txt
 chmod 777 fol_*/*_2.txt
+echo Job Completed
