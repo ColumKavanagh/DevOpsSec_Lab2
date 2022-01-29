e039e4ba627313458092358ad83037ac916bfbca
Colum Kavanagh <colum.j.kavanagh@gmail.com>
2022-01-29 16:01:46 +0000

    adding README.md file

---
+++ README.md
@@ -0,0 +1,5 @@
+# Week 2 Lab – Bash & Git
+Colum Kavanagh 21121851
+[My GitHub repo for DevOpsSec Lab 2](https://github.com/ColumKavanagh/DevOpsSec_Lab2)
+---
+To run the script.sh run: `bash script.sh`
--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -r fol_1
 rm -r fol_2
-mkdir fol_1
-mkdir fol_2
-touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt
-touch fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+mkdir fol_1 fol_2
+touch fol_1/1_1.txt fol_1/1_2.txt fol_1/1_3.txt fol_2/2_1.txt fol_2/2_2.txt fol_2/2_3.txt
+chmod 600 fol_*/*_1.txt fol_*/*_3.txt
+chmod 777 fol_*/*_2.txt
